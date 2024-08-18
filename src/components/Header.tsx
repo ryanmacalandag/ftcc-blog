9c8d380a3e4dd6f88a5acdba3c3f879b39ff63db
Ryan Macalandag <rmacalandag@gmail.com>
2024-08-18 19:20:09 +1000

    install and setup

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -20,6 +20,7 @@ interface MenuItem {
 const menuItems: MenuItem[] = [
   { name: "Blog", href: "/" },
   { name: "About", href: "/about" },
+  { name: "Bhutan", href: "/tag/bhutan" },
 ];
 export const Navigation: FunctionComponent = () => {
   const pathname = usePathname();
